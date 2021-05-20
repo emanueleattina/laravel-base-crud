d7cd7a835306f46bb0c7255c5afc9e255799ffff
emanuele <emattina97@gmail.com>
2021-05-20 17:43:44 +0200

    Index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('main')
+<main>
+    <div class="container">
+        <div class="card-container">
+        @foreach ($comics as $comic)
+            <div class="card">
+                <a href="{{route('comics.show', ['comic' => $comic->id])}}">
+                    <img src="{{$comic->thumb}}" alt="">
+                    <h4>{{$comic->title}}</h4>
+                    <h5>{{$comic->series}}</h5>
+                    <h6>{{$comic->price}}</h6>
+                    <h6>{{$comic->type}}</h6>
+                    <h6>{{$comic->sale_date}}</h6>
+                    {{-- <p>{{$comic->description}}</p> --}}
+                </a>
+            </div>        
+        @endforeach
+        </div>
+    </div>
+</main>
+@endsection
