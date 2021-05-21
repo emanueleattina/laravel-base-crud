817afcb7c531fb902532b21cf72e4455ab312093
emanuele <emattina97@gmail.com>
2021-05-21 17:54:27 +0200

    Store

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,13 +2,15 @@
 
 @section('main')
 <main>
-    <form action="" method="post" action="{{route('comics.store')}}">
+    <form action="{{route('comics.store')}}" method="post">
+        @csrf
+        @method('POST')
         <input type="text" name="title" value="" placeholder="title">
         <input type="text" name="description" value="" placeholder="description">
         <input type="text" name="thumb" value="" placeholder="thumb">
         <input type="text" name="price" value="" placeholder="price">
         <input type="text" name="series" value="" placeholder="series">
-        <input type="text" name="sale_date" value="" placeholder="sale_date">
+        <input type="date" name="sale_date" value="" placeholder="sale_date">
         <input type="text" name="type" value="" placeholder="type">
         <input type="submit" name="" value="submit">
     </form>
